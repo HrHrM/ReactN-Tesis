2f568e2d4febf8eb42482f0a2dd3da1fd9bf38d3
Johnny <johnny130205@gmail.com>
2021-11-18 11:04:08 -0400

    Update Pp.js, added vibration to the buttons
    Add

--- Components/Pp.js
+++ Components/Pp.js
@@ -1,17 +1,21 @@
 import React       from 'react'
-import { Button }  from 'react-native'
+import { 
+    Button,
+    Vibration
+ }                 from 'react-native'
 import * as Speech from 'expo-speech'
 
-export default function Pp({ text = 'te' }) {
+export default function Pp({ text }) {
     const speak = () => {
         Speech.speak(text)
+        Vibration.vibrate(10 * 50)
     }
     return(
         <Button 
             onPress            = {speak}
             title              = {text}
             color              = '#841584'
-            accessibilityLabel = "Learn more about this purple button"
+            accessibilityLabel = "Audio Button"
         />
     )
 }
